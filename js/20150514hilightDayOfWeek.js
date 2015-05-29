f5cbdbd0b8ee87d0e4eb5d0ef6a59befbc1df351
unknown <anton.skakunov@DST-03421.UG.local>
2015-05-29 16:20:55 +0300

    add

--- js/20150514hilightDayOfWeek.js
+++ js/20150514hilightDayOfWeek.js
@@ -1,19 +1,22 @@
  // Plugin with mark current day of week
- // Version 1.00
- // Date: 14.05.2015
+ // Version 1.01
+ // Date: 28.05.2015
 
 jQuery(document).ready(function(){
 	var dayOfWeek = new Date(),
 		variables = {
 			"selector": "table.table td.table",
 			"week": ["Пн","Вт","Ср","Чт","Пт","Сб","Вс"],
+			"weekFool":["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"],
 			"bgcolor": "#EECBAD"
 		};
 
 	dayOfWeek = dayOfWeek.getDay();
 	var dayName = variables.week[dayOfWeek-1];
+	var foolDayName = variables.weekFool[dayOfWeek-1];
 		$(variables.selector).each(function(){
-			if($(this).html().slice(0,2) == dayName){
+			var name = $(this).html();
+			if((name.slice(0,2) == dayName) || (name == foolDayName)){
 				$(this).css("background-color", variables.bgcolor);	
 			}
 	});
